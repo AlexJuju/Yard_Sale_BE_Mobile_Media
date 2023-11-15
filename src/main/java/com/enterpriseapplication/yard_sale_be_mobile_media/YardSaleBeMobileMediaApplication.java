22c98553d5216b8a9b1e987c73a4f97871e64371
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-11-15 18:06:08 +0100

    First Commit.

---
+++ src/main/java/com/enterpriseapplication/yard_sale_be_mobile_media/YardSaleBeMobileMediaApplication.java
@@ -0,0 +1,13 @@
+package com.enterpriseapplication.yard_sale_be_mobile_media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YardSaleBeMobileMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YardSaleBeMobileMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enterpriseapplication/yard_sale_be_mobile_media/YardSaleBeMobileMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterpriseapplication.yard_sale_be_mobile_media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YardSaleBeMobileMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
